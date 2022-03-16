fec281ba3613659602e0f70e7fa6d0947dd5fc44
LoganO3 <92324728+LoganO3@users.noreply.github.com>
2022-03-16 08:55:20 -0700

    Added Some standard assets and a player health script

---
+++ 3D Game Project/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int hp = 5;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        hp -= damage;
+    }
+}
